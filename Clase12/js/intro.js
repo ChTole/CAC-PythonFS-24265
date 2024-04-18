bc56dba8abb7ed8adf1f45f673440d36e430366f
ChTole <ch.ariel.tole@gmail.com>
2024-04-18 09:54:00 -0300

    Agrego clases de Git (10 y 11) y  de intro a JS (12)

---
+++ Clase12/js/intro.js
@@ -0,0 +1,13 @@
+// Comentario de una línea
+
+/* 
+Comentario 
+de mas
+de una línea
+*/
+
+console.log("Desde mi script!");
+
+// alert("Esto pertenece a la ventana(window)!");
+
+console.log(document.querySelector('h1'));
