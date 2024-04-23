3ffbf5cd49c7b12adc207ace3a197f3f0a9edb5c
ChTole <ch.ariel.tole@gmail.com>
2024-04-23 11:37:07 -0300

    Agrego clase 14 - Operadores lógicos y est. condicionales

--- Clase14/js/controlDeFlujo.js
+++ Clase14/js/controlDeFlujo.js
@@ -1,4 +1,35 @@
 // CONTROL DE FLUJO
 // Estructuras condicionales (if - if...else - if...else if...else - switch...case)
+console.log('Comienzo');
+let edad = 170;
+console.log('Edad ingresada:', edad);
+
+// Mayoría de edad
+// edad >= 18    -    edad > 17
+if (edad >= 18) { // cuando la condición devuelva true
+    console.log('Sos mayor de edad.');
+} else { // cuando la condición devuelva false
+    console.log('Sos menor de edad.');
+}
+
+// if (edad < 18) { // redundante e ineficiente
+//     console.log('Sos menor de edad.');
+// }
+
+// Situación para votar (en Argentina)
+if (edad < 16) {
+    console.log('Aún no podés votar.');
+} else if (edad == 16 || edad == 17 || edad > 70) {
+    console.log('Podés votar, pero no es obligatorio.');
+} else {
+    console.log('Es obligatorio tu voto.')
+}
+
+// Tarea: validar el dato (nro entero y en un rango coherente)
+
+
+console.log('Fin del algoritmo');
+
+
 // Estructuras repetitivas indeterminadas (while - do...while)
 // Estructuras repetitivas determinadas (for (desde,hasta,paso) - for...of - for...in)
--- Clase14/js/main.js
+++ Clase14/js/main.js
@@ -22,15 +22,56 @@ esIgual = "4" === 4; // false
 // **************************************************
 // Lógicos: && (conjunción ó «y»), || (disyunción u «ó») y ! (negación -unario-) también devuelven booleanos.
 // **************************************************
-// Conjunción
+// Conjunción &&
+// 1ra proposición: Si el día está lindo,       true    false
+// 2da proposición: «Y» tenemos tiempo,         true    true
+// Resultado:       salimos a caminar.          true    false
+console.log(true && true);  // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); //false
+// Para obtener true, ambos operandos deben ser true.
 
-// Disyunción
+// Disyunción ||  alt 124
+// 1ra proposición: Si vos estás en el shopping,        true    false   true    false
+// 2da proposición: «Ó» yo estoy en el shopping,        false   true    true    false
+// Resultado:       alguno le compra el regalo a mamá.  true    true    true    false
+console.log(true || false); // true
+console.log(false || false); // false
+// Para obtener true, sólo necesito que uno sea true.
 
-// Negación
+// Negación   !
+console.log(!true); // false
+console.log(!false); // true
+
+// PRECEDENCIA DE OPERADORES
+// 1° Aritméticos (teniendo en cuenta su precedencia)
+// 2° De comparación  e identidad
+// 3° Lógicos: -> Negación -> Conjunción -> Disyunción
+let operadoresCombinados = 5 ** 3 > 301 % 3 && !true || "hola" !== "HOLA";
+//                          125   >   1     && !true || "hola" != "HOLA";
+//                              true        && !true ||   true
+//                              true        && false ||   true  = true
+console.log(operadoresCombinados);
 
 // CUADROS DE DIÁLOGO (alert, prompt, confirm)
 // No son de uso habitual, pero en éste punto nos sirven para interactuar con nuestra página, sitio o aplicación.
 // Estos mensajes automatizados tienen dos inconvenientes:
 // No hay una forma estándar de cambiar su aspecto con CSS.
 // Dependen de la configuración regional del navegador, lo que significa que puedes tener una página en un idioma pero un mensaje de error en otro idioma.
+
+// Alerta
+// alert("Mensaje");
+
+// Ingreso de dato
+// let datoIngresado = prompt('Ingrese un número:'); // SIEMPRE DEVUELVE STRING
+// console.log(datoIngresado, typeof datoIngresado);
+
+// datoIngresado = Number(datoIngresado); // "casteo" o conversión del dato a número
+// console.log(datoIngresado, typeof datoIngresado);
+
+// Confirmación
+let confirma = confirm('¿Está de acuerdo?');
+console.log(confirma, typeof confirma);
+
 
